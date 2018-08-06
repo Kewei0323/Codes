c3ecf7e5c0c2542724c3e31b46bee5af6104859b
Kewei0323 <34942131+Kewei0323@users.noreply.github.com>
2018-08-06 20:58:58 +0800

    Update 22. 第一个只出现一次的字符.cpp

--- CodingInterviews/22. 第一个只出现一次的字符.cpp
+++ CodingInterviews/22. 第一个只出现一次的字符.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int FirstNotRepeatingChar(string str) {
+        if(str.size() == 0){
+            return -1;
+        }
         map<char, int> hashmp;
         //put element of array into hashmap
         for(int i = 0; i < str.size(); i++){
