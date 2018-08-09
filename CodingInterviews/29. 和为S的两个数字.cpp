da23c657dda1e32a2b30d17d8b28f6502540cbd2
Kewei0323 <34942131+Kewei0323@users.noreply.github.com>
2018-08-09 22:35:27 +0800

    Create 29. 和为S的两个数字.cpp

---
+++ CodingInterviews/29. 和为S的两个数字.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> FindNumbersWithSum(vector<int> array,int sum) {
+        int len = array.size();
+        int i = 0, j = len - 1;
+        int count = INT_MAX;
+        
+        vector<int> arr2;
+        
+        while(i < j){
+            if(array[i] + array[j] < sum){
+                i++;
+            }else if(array[i] + array[j] > sum){
+                j--;
+            }else{
+                if(count > array[i]*array[j]){
+                    count = array[i]*array[j];
+                    if(!arr2.empty()){
+                        arr2.pop_back();
+                        arr2.pop_back();
+                    }
+                    arr2.push_back(array[i]);
+                    arr2.push_back(array[j]);
+                }
+                i++;
+                j--;
+            }
+        }
+        return arr2;
+    }
+};
